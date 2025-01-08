7e716fac72a6c04c99c1f447c99afb316f73459d
Abdul Rehman Khalid <abdulrehmankhalid906@gmail.com>
2025-01-08 16:39:28 +0500

    Added SMS API

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,23 +12,6 @@
                                     <div class="justify-content-between align-items-center flex-sm-row flex-column gap-10">
                                         <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
                                             <div class="card-title mb-6">
-                                                <h5 class="text-nowrap mb-1">Schools</h5>
-                                            </div>
-                                            <div class="mt-sm-auto">
-                                                <h4 class="mb-0">{{ $data['school'] }}</h4>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="col-12 col-sm-6 col-md-3 mb-4">
-                            <div class="card">
-                                <div class="card-body">
-                                    <div class="justify-content-between align-items-center flex-sm-row flex-column gap-10">
-                                        <div class="d-flex flex-sm-column flex-row align-items-start justify-content-between">
-                                            <div class="card-title mb-6">
                                                 <h5 class="text-nowrap mb-1">Teachers</h5>
                                             </div>
                                             <div class="mt-sm-auto">
