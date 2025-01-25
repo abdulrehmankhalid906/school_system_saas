00a6579cbfdcf4d140b5c9d7b2b6053032215a1d
Abdul Rehman Khalid <abdulrehmankhalid906@gmail.com>
2025-01-25 14:10:58 +0500

    check for the history

--- resources/views/schools/billing_history.blade.php
+++ resources/views/schools/billing_history.blade.php
@@ -30,7 +30,7 @@
                                         <div class="plan-statistics">
                                             <div class="d-flex justify-content-between">
                                                 <h6 class="mb-1">Days</h6>
-                                                <h6 class="mb-1">{{ $subscription['used_days'] }} of Days {{ $subscription['total_days'] }}</h6>
+                                                <h6 class="mb-1">{{ $subscription['used_days'] ?? '0' }} of {{ $subscription['total_days'] ?? '0' }} Days</h6>
                                             </div>
                                             <div class="progress rounded mb-1">
                                                 <div class="progress-bar rounded" role="progressbar" style="width: {{ $subscription['progress_percentage'] }}%;" aria-valuenow="{{ $subscription['progress_percentage'] }}" aria-valuemin="0" aria-valuemax="100"></div>
@@ -44,37 +44,39 @@
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 mb-4">
-                        <div class="card">
-                            <h5 class="card-header">Billing History</h5>
-                            <div class="card-body">
-                                <table id="example" class="table dt-responsive nowrap w-100">
-                                    <thead>
-                                        <tr>
-                                            <th>Package</th>
-                                            <th>Package Type</th>
-                                            <th>Payment Method</th>
-                                            <th>Amount</th>
-                                            <th>Validity / Days</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        @foreach ($subscriptionHistory as $subHistory)
-                                            <tr id="id-{{ $subHistory->id }}">
-                                                <td>{{ Str::upper($subHistory->type ?? '') }}</td>
-                                                <td>{{ Str::upper($subHistory->usage_type ?? '') }}</td>
-                                                <td>{{ Str::upper($subHistory->payment_method ?? '') }}</td>
-                                                <td>{!! $subHistory->amount != null ? "<span class='badge bg-success'>Paid</span>" : '' !!}
-                                                <td>{{ $subHistory->end_date ?? '' }} / {{ $subHistory->duration ?? '' }}</td>
+                @if(count($subscriptionHistory) > 0)
+                    <div class="row">
+                        <div class="col-12 col-sm-12 col-md-12 mb-4">
+                            <div class="card">
+                                <h5 class="card-header">Billing History</h5>
+                                <div class="card-body">
+                                    <table id="example" class="table dt-responsive nowrap w-100">
+                                        <thead>
+                                            <tr>
+                                                <th>Package</th>
+                                                <th>Package Type</th>
+                                                <th>Payment Method</th>
+                                                <th>Amount</th>
+                                                <th>Validity / Days</th>
                                             </tr>
-                                       @endforeach
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            @foreach ($subscriptionHistory as $subHistory)
+                                                <tr id="id-{{ $subHistory->id }}">
+                                                    <td>{{ Str::upper($subHistory->type ?? '') }}</td>
+                                                    <td>{{ Str::upper($subHistory->usage_type ?? '') }}</td>
+                                                    <td>{{ Str::upper($subHistory->payment_method ?? '') }}</td>
+                                                    <td>{!! $subHistory->amount != null ? "<span class='badge bg-success'>Paid</span>" : '' !!}
+                                                    <td>{{ $subHistory->end_date ?? '' }} / {{ $subHistory->duration ?? '' }}</td>
+                                                </tr>
+                                        @endforeach
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                @endif
             </div>
         </div>
     </div>
