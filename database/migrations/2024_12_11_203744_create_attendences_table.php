2ad383ddbee1617da2c6726ddaaeb03b07ecde24
Abdul Rehman Khalid <abdulrehmankhalid906@gmail.com>
2025-02-19 17:52:06 +0500

    fixed attendance table

--- database/migrations/2024_12_11_203744_create_attendences_table.php
+++ database/migrations/2024_12_11_203744_create_attendences_table.php
@@ -13,10 +13,10 @@ return new class extends Migration
     {
         Schema::create('attendences', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('klass_id')->constrained();
-            $table->foreignId('section_id')->constrained();
-            $table->enum('status',['absent','leave']);
+            $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('klass_id')->nullable()->constrained('klasses')->onDelete('cascade');
+            $table->foreignId('section_id')->nullable()->constrained('sections')->onDelete('cascade');
+            $table->enum('status',['present','absent','leave']);
             $table->date('date');
             $table->timestamps();
         });
